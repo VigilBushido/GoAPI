1580157c3fb721b5e21fbeda8f4dbd4b8499abf6
VigilBushido <vigilbushido@gmail.com>
2025-04-25 23:56:09 -0400

    +route about page

---
+++ react-movies/app/routes/about.tsx
@@ -0,0 +1,12 @@
+import type { Route } from "./+types/home";
+
+export function meta({ }: Route.MetaArgs) {
+    return [
+        { title: "New React Router App" },
+        { name: "description", content: "Welcome" }
+    ];
+}
+
+export default function Home() {
+    return <main>About</main>;
+}
