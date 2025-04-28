314c52fb088e3ea7edf9db15232c60364eb2c08c
VigilBushido <vigilbushido@gmail.com>
2025-04-27 23:53:24 -0400

    + movies page

--- react-movies/app/routes/actors.tsx
+++ react-movies/app/routes/actors.tsx
@@ -2,8 +2,8 @@ import type { Route } from "./+types/home";
 
 export function meta({ }: Route.MetaArgs) {
     return [
-        { title: "New React Router App" },
-        { name: "description", content: "Actor Information" }
+        { title: "Movies App" },
+        { name: "description", content: "Welcome to Movies App Router!" },
     ];
 }
 
--- react-movies/app/routes/movies.tsx
+++ react-movies/app/routes/movies.tsx
@@ -1,6 +1,12 @@
-const movies = () => {
-    return (
-        <div>movies</div>
-    );
-};
-export default movies;
+import type { Route } from "./+types/home";
+
+export function meta({ }: Route.MetaArgs) {
+    return [
+        { title: "Movies App" },
+        { name: "description", content: "Welcome to Movies App Router!" },
+    ];
+}
+
+export default function Home() {
+    return <main>Movies</main>;
+}
